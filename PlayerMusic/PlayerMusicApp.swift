fb60ccc1a9a77938de97b0377179ab58945c2fa8
ForegroundMattia <mmarranzino23@fed.idserve.net>
2023-11-23 10:44:00 +0100

    try

---
+++ PlayerMusic/PlayerMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerMusicApp.swift
+//  PlayerMusic
+//
+//  Created by Mattia Marranzino on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayerMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
